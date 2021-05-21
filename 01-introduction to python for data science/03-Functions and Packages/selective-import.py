24095b7be2210d62fe1141eb30543b2a114e1729
Racella Lc <rasilamichhane@gmail.com>
2021-05-21 22:26:04 +0545

    functions

---
+++ 01-introduction to python for data science/03-Functions and Packages/selective-import.py
@@ -0,0 +1,16 @@
+# Perform a selective import from the math package where you only import the radians function.
+# Calculate the distance travelled by the Moon over 12 degrees of its orbit.
+# Assign the result to dist. You can calculate this as r * phi, where r is the radius
+# and phi is the angle in radians. To convert an angle in degrees to an angle in radians,
+# use the radians() function, which you just imported.
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+from math import radians
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = radians(12) * r
+
+# Print out dist
+print(dist)
